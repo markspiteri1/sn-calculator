48b6627de8808ac5ac58c47a5b40061e4d22a83c
Mark Spiteri <markspiteri1@gmail.com>
2020-06-06 15:17:27 +0200

    feat: add prop on button to distinguish and style function button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,9 +3,10 @@ import './button.scss'
 
 interface ButtonProps {
     value: string;
-    onClick: (e:any) => void;
+    isFunction?: boolean;
+    onClick: (e: any) => void;
 }
 
-const Button = (props: ButtonProps) => <div className='button' onClick={props.onClick}>{props.value}</div>
+const Button = (props: ButtonProps) => <div className={props.isFunction && (props.isFunction === true) ? 'button function' : 'button'} onClick={props.onClick}>{props.value}</div>
 
 export default Button
--- src/components/calculator/calculator.tsx
+++ src/components/calculator/calculator.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Button from './../button/button'
 import './calculator.scss'
+import { isFunction } from 'util'
 
 const Calculator = () => {
     const [displayValue, setDisplayValue] = useState('0')
@@ -15,7 +16,7 @@ const Calculator = () => {
 
         if (response.ok) {
             let num = await response.text();
-            setDisplayValue(displayValue + parseInt(num))
+            setDisplayValue(parseInt(num) +'')
         } else {
             console.log("HTTP-Error: " + response.status);
         }
@@ -24,38 +25,38 @@ const Calculator = () => {
     return (<div className="calc-container">
         <div className="value">{displayValue}</div>
         <div className="row">
-            <Button value='sin' onClick={e => handleClick(e)} />
-            <Button value='cos' onClick={e => handleClick(e)} />
-            <Button value='tan' onClick={e => handleClick(e)} />
-            <Button value='/' onClick={e => handleClick(e)} />
+            <Button value='sin' onClick={e => handleClick(e)} isFunction/>
+            <Button value='cos' onClick={e => handleClick(e)} isFunction/>
+            <Button value='tan' onClick={e => handleClick(e)} isFunction/>
+            <Button value='/' onClick={e => handleClick(e)} isFunction/>
         </div>
         <div className="row">
             <Button value='7' onClick={e => handleClick(e)} />
             <Button value='8' onClick={e => handleClick(e)} />
             <Button value='9' onClick={e => handleClick(e)} />
-            <Button value='X' onClick={e => handleClick(e)} />
+            <Button value='X' onClick={e => handleClick(e)} isFunction />
         </div>
         <div className="row">
             <Button value='4' onClick={e => handleClick(e)} />
             <Button value='5' onClick={e => handleClick(e)} />
             <Button value='6' onClick={e => handleClick(e)} />
-            <Button value='-' onClick={e => handleClick(e)} />
+            <Button value='-' onClick={e => handleClick(e)} isFunction />
         </div>
         <div className="row">
             <Button value='1' onClick={e => handleClick(e)} />
             <Button value='2' onClick={e => handleClick(e)} />
             <Button value='3' onClick={e => handleClick(e)} />
-            <Button value='+' onClick={e => handleClick(e)} />
+            <Button value='+' onClick={e => handleClick(e)} isFunction/>
         </div>
         <div className="row">
             <Button value='0' onClick={e => handleClick(e)} />
             <Button value='.' onClick={e => handleClick(e)} />
-            <Button value='^2' onClick={e => handleClick(e)} />
-            <Button value='=' onClick={e => handleClick(e)} />
+            <Button value='^2' onClick={e => handleClick(e)}/>
+            <Button value='=' onClick={e => handleClick(e)} isFunction/>
         </div>
         <div className="row">
-            <Button value='RAND' onClick={() => generateRandomNumber()} />
-            <Button value='C' onClick={() => setDisplayValue('')} />
+            <Button value='RAND' onClick={() => generateRandomNumber()} isFunction/>
+            <Button value='C' onClick={() => setDisplayValue('0')} isFunction/>
         </div>
     </div>)
 }
