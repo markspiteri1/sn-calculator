87006621d7fbf0f47e7b3d1f52283ab1fc5ca81d
Mark Spiteri <markspiteri1@gmail.com>
2020-06-06 13:24:22 +0200

    feat: add onClick function button prop

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './button.scss'
 
 interface ButtonProps {
-    value: string
+    value: string;
+    onClick: (e:any) => void;
 }
 
-const Button = (props: ButtonProps) => <div className='button'>{props.value}</div>
+const Button = (props: ButtonProps) => <div className='button' onClick={props.onClick}>{props.value}</div>
 
 export default Button
--- src/components/calculator/calculator.tsx
+++ src/components/calculator/calculator.tsx
@@ -4,37 +4,40 @@ import './calculator.scss'
 
 const Calculator = () => {
     const [displayValue, setDisplayValue] = useState('0')
+
+    const handleClick = (e: any) => { debugger;alert(e.currentTarget.innerText) }
+
     return (<div className="calc-container">
         <div className="value">{displayValue}</div>
         <div className="row">
-            <Button value='sin'/>
-            <Button value='cos'/>
-            <Button value='tan'/>
-            <Button value='/'/>
+            <Button value='sin' onClick={e => handleClick(e)} />
+            <Button value='cos' onClick={e => handleClick(e)} />
+            <Button value='tan' onClick={e => handleClick(e)} />
+            <Button value='/' onClick={e => handleClick(e)} />
         </div>
         <div className="row">
-            <Button value='7'/>
-            <Button value='8'/>
-            <Button value='9'/>
-            <Button value='X'/>
+            <Button value='7' onClick={e => handleClick(e)} />
+            <Button value='8' onClick={e => handleClick(e)} />
+            <Button value='9' onClick={e => handleClick(e)} />
+            <Button value='X' onClick={e => handleClick(e)} />
         </div>
         <div className="row">
-            <Button value='4'/>
-            <Button value='5'/>
-            <Button value='6'/>
-            <Button value='-'/>
+            <Button value='4' onClick={e => handleClick(e)} />
+            <Button value='5' onClick={e => handleClick(e)} />
+            <Button value='6' onClick={e => handleClick(e)} />
+            <Button value='-' onClick={e => handleClick(e)} />
         </div>
         <div className="row">
-            <Button value='1'/>
-            <Button value='2'/>
-            <Button value='3'/>
-            <Button value='+'/>
+            <Button value='1' onClick={e => handleClick(e)} />
+            <Button value='2' onClick={e => handleClick(e)} />
+            <Button value='3' onClick={e => handleClick(e)} />
+            <Button value='+' onClick={e => handleClick(e)} />
         </div>
         <div className="row">
-            <Button value='0'/>
-            <Button value='.'/>
-            <Button value='^2'/>
-            <Button value='='/>
+            <Button value='0' onClick={e => handleClick(e)} />
+            <Button value='.' onClick={e => handleClick(e)} />
+            <Button value='^2' onClick={e => handleClick(e)} />
+            <Button value='=' onClick={e => handleClick(e)} />
         </div>
     </div>)
 }
